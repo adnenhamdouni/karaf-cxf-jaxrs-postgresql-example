3539cf4b44a769e63ca83ddd73df6eb42fc11016
adnen hamdouni <adnen.hamdouni@squeezer-software.com>
2015-08-20 15:23:28 +0100

    work with separetly postgres acces and web service CXF

--- hello-rest/src/main/java/com/hello/rest/HelloRestServiceImpl.java
+++ hello-rest/src/main/java/com/hello/rest/HelloRestServiceImpl.java
@@ -1,37 +1,20 @@
 package com.hello.rest;
 
-import net.lr.tutorial.karaf.db.examplejpa.Person;
-import net.lr.tutorial.karaf.db.examplejpa.PersonService;
-
 import com.hello.service.impl.HelloService;
 
 public class HelloRestServiceImpl implements HelloRestService {
 
-	// Just like Spring. Please add Getters/Setters. Blueprint annotations are
-	// still work in progress
 	private HelloService helloService;
 
-//	private PersonService personService;
-
 	public String handleGet(String name) {
-
-//		Person person = new Person("x", "@x");
-//		personService.add(person);
 
 		return helloService.sayHello(name);
 
 	}
 
-	/*
-	 * Constructor
-	 */
-
 	public HelloRestServiceImpl() {
 	}
 
-	/*
-	 * Getters and Setters
-	 */
 
 	public HelloService getHelloService() {
 		return helloService;
@@ -41,12 +24,5 @@ public class HelloRestServiceImpl implements HelloRestService {
 		this.helloService = helloService;
 	}
 
-//	public void setPersonService(PersonService personService) {
-//		this.personService = personService;
-//	}
-//
-//	public PersonService getPersonService() {
-//		return personService;
-//	}
 
 }

d5f93bf4336464308c24bf27a796dd85900451c5
adnen hamdouni <adnen.hamdouni@squeezer-software.com>
2015-08-20 15:25:35 +0100

    work with postgres database access called in web service implementation

--- hello-rest/src/main/java/com/hello/rest/HelloRestServiceImpl.java
+++ hello-rest/src/main/java/com/hello/rest/HelloRestServiceImpl.java
@@ -7,31 +7,22 @@ import com.hello.service.impl.HelloService;
 
 public class HelloRestServiceImpl implements HelloRestService {
 
-	// Just like Spring. Please add Getters/Setters. Blueprint annotations are
-	// still work in progress
 	private HelloService helloService;
 
-//	private PersonService personService;
+	private PersonService personService;
 
 	public String handleGet(String name) {
 
-//		Person person = new Person("x", "@x");
-//		personService.add(person);
+		Person person = new Person("x", "@x");
+		personService.add(person);
 
 		return helloService.sayHello(name);
 
 	}
 
-	/*
-	 * Constructor
-	 */
-
 	public HelloRestServiceImpl() {
 	}
 
-	/*
-	 * Getters and Setters
-	 */
 
 	public HelloService getHelloService() {
 		return helloService;
@@ -41,12 +32,12 @@ public class HelloRestServiceImpl implements HelloRestService {
 		this.helloService = helloService;
 	}
 
-//	public void setPersonService(PersonService personService) {
-//		this.personService = personService;
-//	}
-//
-//	public PersonService getPersonService() {
-//		return personService;
-//	}
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+	}
+
+	public PersonService getPersonService() {
+		return personService;
+	}
 
 }
